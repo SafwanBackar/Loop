17b8d7f42adaa30bf0207e615f546065f7e21ed4
SafwanBackar <sbackar29@gmail.com>
2022-02-15 10:48:17 +0530

    Loop

---
+++ while/main.js
@@ -0,0 +1,31 @@
+
+// var num = -10;
+// while (num <= 19) {
+//     console.log(num);
+//     num++;
+// }
+
+// var num = 10;
+
+// while (num < 41) {
+//     console.log(num);
+//     num += 2;
+// }
+
+var num = 301;
+
+// while (num < 33) {
+//     if (num % 2 !== 0) {
+//         console.log(num);
+//     }
+//     num++;
+
+// }
+
+var num = 6;
+while (num < 50) {
+    if (num % 5 == 0 && num % 3 == 0) {
+        console.log(num);
+    }
+    num++;
+}
